3952d33bda2ea0fdba611c024fae7491e197efa5
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-17 00:51:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  var newArray = new Array();
+  var newArray = [];
   newArray = array;
   newArray.unshift(element);
   return newArray;
